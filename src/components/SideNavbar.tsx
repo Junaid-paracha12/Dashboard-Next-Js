54f1954081985b0eccb03b60d8415f3d41ab8a6e
Junaid-paracha-007 <junaidmaqbool009@gmail.com>
2024-05-21 20:14:21 +0500

    update layout,SideNavbar, Add dropdown-menu

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -14,7 +14,14 @@ import {
   Trash2,
   ChevronRight,
   LayoutDashboard,
+  MenuIcon,
 } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Button } from "./ui/button";
 
 export default function SideNavbar({}: Props) {
@@ -24,70 +31,140 @@ export default function SideNavbar({}: Props) {
     setIsCollapsed(!isCollapsed);
   }
   return (
-    <div className="relative min-w-[80px] border-r px-3 pb-7 pt-20">
-      <div className="absolute right-0 top-7">
-        <Button
-          onClick={toggleSidebar}
-          variant="secondary"
-          className="hidden sm:flex rounded-full p-2"
-        >
-          <ChevronRight />
-        </Button>
+    <>
+      <div className="hidden sm:grid fixed min-w-[80px] border-r px-3  pt-7">
+      <h1 className="text-2xl">Logo</h1>
+        <div className="absolute right-0 top-7">
+          {/* <Button
+            onClick={toggleSidebar}
+            variant="secondary"
+            className=" rounded-full p-2"
+          >
+            <ChevronRight />
+          </Button> */}
+        </div>
+        <div className="mt-7">
+        <Nav
+          isCollapsed={isCollapsed}
+          links={[
+            {
+              title: "Dashboard",
+              label: "",
+              icon: LayoutDashboard,
+              variant: "default",
+              href: "/",
+            },
+            {
+              title: "Inbox",
+              label: "128",
+              icon: Inbox,
+              variant: "ghost",
+              href: "/inbox",
+            },
+            {
+              title: "Drafts",
+              label: "9",
+              icon: File,
+              variant: "ghost",
+              href: "/draft",
+            },
+            {
+              title: "Sent",
+              label: "",
+              icon: Send,
+              variant: "ghost",
+              href: "/sent",
+            },
+            {
+              title: "Junk",
+              label: "23",
+              icon: ArchiveX,
+              variant: "ghost",
+              href: "/",
+            },
+            {
+              title: "Trash",
+              label: "",
+              icon: Trash2,
+              variant: "ghost",
+              href: "/",
+            },
+            {
+              title: "Archive",
+              label: "",
+              icon: Archive,
+              variant: "ghost",
+              href: "/",
+            },
+          ]}
+        /></div>
       </div>
-      <Nav
-        isCollapsed={isCollapsed}
-        links={[
-          {
-            title: "Dashboard",
-            label: "",
-            icon: LayoutDashboard,
-            variant: "default",
-            href: "/",
-          },
-          {
-            title: "Inbox",
-            label: "128",
-            icon: Inbox,
-            variant: "ghost",
-            href: "/inbox",
-          },
-          {
-            title: "Drafts",
-            label: "9",
-            icon: File,
-            variant: "ghost",
-            href: "/draft",
-          },
-          {
-            title: "Sent",
-            label: "",
-            icon: Send,
-            variant: "ghost",
-            href: "/sent",
-          },
-          {
-            title: "Junk",
-            label: "23",
-            icon: ArchiveX,
-            variant: "ghost",
-            href: "/",
-          },
-          {
-            title: "Trash",
-            label: "",
-            icon: Trash2,
-            variant: "ghost",
-            href: "/",
-          },
-          {
-            title: "Archive",
-            label: "",
-            icon: Archive,
-            variant: "ghost",
-            href: "/",
-          },
-        ]}
-      />
-    </div>
+
+      <div className="absolute sm:hidden top-5 right-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MenuIcon />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-white z-50 h-full overflow-y-auto">
+            <DropdownMenuItem>
+              <Nav
+               isCollapsed={false}
+                links={[
+                  {
+                    title: "Dashboard",
+                    label: "",
+                    icon: LayoutDashboard,
+                    variant: "default",
+                    href: "/",
+                  },
+                  {
+                    title: "Inbox",
+                    label: "128",
+                    icon: Inbox,
+                    variant: "ghost",
+                    href: "/inbox",
+                  },
+                  {
+                    title: "Drafts",
+                    label: "9",
+                    icon: File,
+                    variant: "ghost",
+                    href: "/draft",
+                  },
+                  {
+                    title: "Sent",
+                    label: "",
+                    icon: Send,
+                    variant: "ghost",
+                    href: "/sent",
+                  },
+                  {
+                    title: "Junk",
+                    label: "23",
+                    icon: ArchiveX,
+                    variant: "ghost",
+                    href: "/",
+                  },
+                  {
+                    title: "Trash",
+                    label: "",
+                    icon: Trash2,
+                    variant: "ghost",
+                    href: "/",
+                  },
+                  {
+                    title: "Archive",
+                    label: "",
+                    icon: Archive,
+                    variant: "ghost",
+                    href: "/",
+                  },
+                ]}
+              />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </>
   );
 }
